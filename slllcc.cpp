999c0c26cfc27eb8f048ea03d2f6704b964a0cdf
setyaki <34120275+setyaki@users.noreply.github.com>
2018-04-29 19:52:41 +0700

    Add files via upload

---
+++ slllcc.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+
+class node{
+	public:
+	int data;
+	node* next;
+};
+
+node* head;
+
+void init()
+{
+	head = NULL;
+}
+
+bool jikakosong()
+{
+	if (head == NULL)
+	return 1;
+	else return 0;
+}
+
+
+
